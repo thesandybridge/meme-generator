3b676a2979aab281bc9e9e333ec0935b63e4930a
Matt Miller <mattmillerf@pm.me>
2023-04-06 15:42:09 -0400

    enable cors

--- next.config.js
+++ next.config.js
@@ -6,8 +6,7 @@ const nextConfig = {
     async headers() {
         return [
             {
-                // matching all API routes
-                source: "/api/:path*",
+                source: "/(.*)",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
